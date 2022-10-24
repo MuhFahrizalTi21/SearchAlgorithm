8006afc6f2c35230c8f6ffdc0ce5d1d3f5349b8b
Muhammad  Fahrizal <m.fahrizal.ft21@mail.umy.ac.id>
2022-10-24 16:27:35 +0700

    Add first commit & add Method input

--- SearchAlgorithm_126_D/Program.cs
+++ SearchAlgorithm_126_D/Program.cs
@@ -8,7 +8,38 @@ namespace SearchAlgorithm_126_D
 {
     class Program
     {
-        // array
+        // array to be searched
+        int[] arr = new int [20];
+        // Number of elements in the array 
+        int n;
+        //Get the number of elements to store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\n Array Should Have Minimum 1 and Maximum 20 elements.\n");
+            }
+            //accept array elements
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Enter Array  Elements ");
+            Console.WriteLine("----------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("<" +(i + 1) +  ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
+
         static void Main(string[] args)
         {
         }
